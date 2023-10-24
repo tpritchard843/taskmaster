1cbdfaa148daf9bbcd6a938bbda8726d0b940fc2
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-10-24 18:26:55 -0400

    Controller now filters todos by completed status

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,14 +1,18 @@
-const Todo = require('../models/Todo')
+const Todo = require('../models/Todo');
 
 module.exports = {
     getTodos: async (req,res)=>{
-        console.log(req.user)
+        //console.log(req.user);
         try{
-            const todoItems = await Todo.find({userId:req.user.id})
-            const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+            const itemsLeft = await Todo.find({userId:req.user.id, completed: false});
+            const itemsCompleted = await Todo.find({userId:req.user.id, completed: true});
+            // console.log({
+            //     'itemsLeft': itemsLeft,
+            //     'itemsCompleted': itemsCompleted
+            // })
+            res.render('todos.ejs', {left: itemsLeft,completed: itemsCompleted,user: req.user});
         }catch(err){
-            console.log(err)
+            console.log(err);
         }
     },
     createTodo: async (req, res)=>{
@@ -20,22 +24,24 @@ module.exports = {
                 dateCreated: Date(),
                 dateCompleted: null
             })
-            console.log('Todo has been added!')
-            res.redirect('/todos')
+            console.log('Todo has been added!');
+            res.redirect('/todos');
         }catch(err){
-            console.log(err)
+            console.log(err);
         }
     },
     markComplete: async (req, res)=>{
+        console.log(req.body.todoIdFromJSFile)
         try{
             await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
                 completed: true,
                 dateCompleted: Date()
             })
-            console.log('Marked Complete')
-            res.json('Marked Complete')
+            console.log('Marked Complete');
+            //res.redirect('/todos');
+            res.json('Marked complete')
         }catch(err){
-            console.log(err)
+            console.log(err);
         }
     },
     markIncomplete: async (req, res)=>{
@@ -43,21 +49,23 @@ module.exports = {
             await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
                 completed: false,
                 dateCompleted: null
-            })
-            console.log('Marked Incomplete')
-            res.json('Marked Incomplete')
+            });
+            console.log('Marked Incomplete');
+            //res.redirect('/todos');
+            res.json('marked incomplete')
         }catch(err){
-            console.log(err)
+            console.log(err);
         }
     },
     deleteTodo: async (req, res)=>{
-        console.log(req.body.todoIdFromJSFile)
+        console.log(req.body.todoIdFromJSFile);
         try{
-            await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile})
-            console.log('Deleted Todo')
-            res.json('Deleted It')
+            await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile});
+            console.log('Deleted Todo');
+            // res.redirect('/todos');
+            res.json('deleted todo')
         }catch(err){
-            console.log(err)
+            console.log(err);
         }
     }
 }
